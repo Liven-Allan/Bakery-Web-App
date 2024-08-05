141ed8f5e2f4589075529bdeae0c0076bea11d23
liven <liven@debian.liven>
2024-08-06 00:19:24 +0300

    Inventory Component

--- bakery_project/inventory/urls.py
+++ bakery_project/inventory/urls.py
@@ -1,12 +1,12 @@
 # inventory/urls.py
 
 from django.urls import path
-from .views import InventoryItemListCreate, InventoryTransactionListCreate,  InventoryItemDetailView
+from .views import InventoryItemListCreate, InventoryTransactionListCreate,  InventoryItemDetailView, historical_data
 
 
 urlpatterns = [
    path('inventory/', InventoryItemListCreate.as_view(), name='inventory-list-create'),
    path('inventory/<int:pk>/', InventoryItemDetailView.as_view(), name='inventory-detail'),  # Added URL pattern for delete view
    path('transactions/', InventoryTransactionListCreate.as_view(), name='transactions-list-create'),
-    
+   path('historical-data/', historical_data, name='historical-data'),
 ]
--- bakery_project/inventory/views.py
+++ bakery_project/inventory/views.py
@@ -4,6 +4,13 @@ from rest_framework import generics
 from .models import InventoryItem, InventoryTransaction
 from .serializers import InventoryItemSerializer, InventoryTransactionSerializer
 
+#chart
+from django.db.models import Sum, F
+from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from django.utils.dateparse import parse_datetime
+import datetime
+
 # add item to inventory
 class InventoryItemListCreate(generics.ListCreateAPIView):
     queryset = InventoryItem.objects.all()
@@ -18,3 +25,26 @@ class InventoryTransactionListCreate(generics.ListCreateAPIView):
 class InventoryItemDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = InventoryItem.objects.all()
     serializer_class = InventoryItemSerializer
+
+#chart
+@api_view(['GET'])
+def historical_data(request):
+    # Get all transactions with related inventory items
+    transactions = InventoryTransaction.objects.select_related('product').all()
+
+    # Format data for each transaction
+    result = []
+    for transaction in transactions:
+        date = transaction.transaction_date.date()
+        quantity = transaction.quantity
+        unit_price = transaction.product.unit_price
+        
+        # Add data to the result list
+        result.append({
+            'date': date.strftime('%Y-%m-%d'),
+            'product': transaction.product.name,
+            'quantity': quantity,
+            'unit_price': str(unit_price)  # Ensure unit_price is a string
+        })
+
+    return JsonResponse(result, safe=False)
